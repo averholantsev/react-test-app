16ca91a3c3bcd79dfbdd6b148b49715e2c2d9cdf
Artem Verkholantsev <averholantsev@ics.perm.ru>
2019-12-04 16:50:51 +0500

    fix

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -9,7 +9,7 @@ const Person = props => {
     return (
         <div className="person">
             <h2 onClick={ props.click }>Hello my name is { props.name } and my age is { props.age }</h2>
-            <p style={ style_child }>{props.children}</p>
+            <p style={ style_child }>{ props.children }</p>
             <input type="text" onChange={ props.changed } value={ props.name } />
         </div>
     );
