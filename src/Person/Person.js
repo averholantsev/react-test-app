66893d188f93c12f9f4a3001bf8f0f3ce79c2c31
Artem Verkholantsev <averholantsev@ics.perm.ru>
2019-12-06 18:18:24 +0500

    fix

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,14 +3,9 @@ import './Person.css'
 import Radium from 'radium';
 
 const Person = props => {
-  const style_child = {
-    fontStyle: 'italic'
-  }
-
   return (
     <div className="person">
       <h2 onClick={ props.click }>Hello my name is { props.name } and my age is { props.age }</h2>
-      <p style={ style_child }>{ props.children }</p>
       <input type="text" onChange={ props.changed } value={ props.name } />
     </div>
   );
