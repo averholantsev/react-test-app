40717f4c5f554965f933640793cc4334b1318bdd
Artem Verkholantsev <averholantsev@ics.perm.ru>
2019-12-06 17:12:08 +0500

    cosmetic

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
               return(
                 <Person 
                   click={ () => this.deletePersonHandler(index) }
-                  name={ person.name } 
+                  name={ person.name }
                   age={ person.age }
                   key={ person.id }
                   changed={ (event) => this.nameChangedHandler(event, person.id) } />
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,17 +2,17 @@ import React from 'react'
 import './Person.css'
 
 const Person = props => {
-    const style_child = {
-        fontStyle: 'italic'
-    }
+  const style_child = {
+    fontStyle: 'italic'
+  }
 
-    return (
-        <div className="person">
-            <h2 onClick={ props.click }>Hello my name is { props.name } and my age is { props.age }</h2>
-            <p style={ style_child }>{ props.children }</p>
-            <input type="text" onChange={ props.changed } value={ props.name } />
-        </div>
-    );
+  return (
+    <div className="person">
+      <h2 onClick={ props.click }>Hello my name is { props.name } and my age is { props.age }</h2>
+      <p style={ style_child }>{ props.children }</p>
+      <input type="text" onChange={ props.changed } value={ props.name } />
+    </div>
+  );
 }
 
 export default Person;
