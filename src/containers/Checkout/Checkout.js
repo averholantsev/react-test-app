af8b4df738401f06abad087a7ed8c3cd1284bc7e
Artem Verkholantsev <averholantsev@ics.perm.ru>
2020-02-28 18:44:23 +0500

    redirect fix

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,8 +4,13 @@ import { connect } from "react-redux";
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
+import * as actions from "../../store/actions/index";
 
 class Checkout extends Component {
+  componentDidMount() {
+    this.props.onInitPurchase();
+  }
+
   checkoutCancelledHandled = () => {
     this.props.history.goBack();
   };
@@ -17,8 +22,10 @@ class Checkout extends Component {
   render() {
     let summary = <Redirect to="/" />;
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandled}
@@ -37,8 +44,15 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   };
 };
 
-export default connect(mapStateToProps)(Checkout);
+const mapDispatchToProps = dispatch => {
+  return {
+    onInitPurchase: () => dispatch(actions.purchaseInit())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
