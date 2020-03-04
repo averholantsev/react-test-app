9cd45b1172183bbd5a307bc51e0e041f81127985
averholantsev <verkholantsevad@gmail.com>
2020-03-05 00:41:23 +0500

    auth with firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,6 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
+import { AUTH_API_KEY } from '../../App.config'
+import axios from "axios";
 
 export const authStart = () => {
   return {
@@ -6,23 +8,40 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = authData => {
   return {
     type: actionTypes.AUTH_SUCCESS,
     authData: authData
   };
 };
 
-export const authFail = (error) => {
+export const authFail = error => {
   return {
     type: actionTypes.AUTH_FAIL,
     error: error
   };
 };
 
-export const auth = () => { //email, password
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
-
-  }
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${AUTH_API_KEY}`;
+    if (!isSignup) {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${AUTH_API_KEY}`;
+    }
+    axios
+      .post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(error => {
+        dispatch(authFail(error));
+      });
+  };
 };
