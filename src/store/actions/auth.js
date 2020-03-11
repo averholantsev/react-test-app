099a5173bd652f4f6adb05f8a38471660129f7e4
Artem Verkholantsev <averholantsev@ics.perm.ru>
2020-03-11 11:33:46 +0500

    bug fix

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -89,15 +89,15 @@ export const authCheckState = () => {
     } else {
       const expirationDate = new Date(localStorage.getItem("expirationDate"));
       if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
         const userId = localStorage.getItem("userId");
         dispatch(authSuccess(token, userId));
         dispatch(
           checkAuthTimeout(
-            expirationDate.getSeconds() - new Date().getSeconds()
+            (expirationDate.getTime() - new Date().getTime())/1000
           )
         );
-      } else {
-        dispatch(logout());
       }
     }
   };
